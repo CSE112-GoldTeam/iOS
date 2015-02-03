5e0b135bd316ccdde18002f1ee9a4fc4490a6778
Ryan Khalili <rykhalil4@gmail.com>
2015-02-02 18:17:02 -0800

    Initial Commit

---
+++ RoboBetty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoboBetty
+//
+//  Created by Ryan Khalili on 2/2/15.
+//  Copyright (c) 2015 CSE112-GoldTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
