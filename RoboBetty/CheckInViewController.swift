af843677211b700af5dd3d120f506f14cdc8cece
jasondizon <jasonmdizon@gmail.com>
2015-02-09 15:31:07 -0800

    some work done on checkIn view

---
+++ RoboBetty/CheckInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CheckInViewController.swift
+//  RoboBetty
+//
+//  Created by Jason Dizon on 2/9/15.
+//  Copyright (c) 2015 CSE112-GoldTeam. All rights reserved.
+//
+
+import UIKit
+
+class CheckInViewController: UIViewController {
+
+    @IBOutlet weak var codeInstruction: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        codeInstruction.alpha = 0.5
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
