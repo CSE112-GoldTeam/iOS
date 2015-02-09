51e72c45957478758274205d96c93a3b902f9ec6
jasondizon <jasonmdizon@gmail.com>
2015-02-09 14:56:18 -0800

    started opening view. needs to be adjusted to fit in landscape mode only. idk how to use constraints lol

--- RoboBetty/ViewController.swift
+++ RoboBetty/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var checkInButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkInButton.backgroundColor = UIColor (red:0.29, green:0.863, blue:0.369, alpha:1)
+        checkInButton.layer.borderColor = UIColor.whiteColor().CGColor
+        checkInButton.layer.cornerRadius = 5
+        checkInButton.layer.borderWidth = 1
         // Do any additional setup after loading the view, typically from a nib.
     }
 
